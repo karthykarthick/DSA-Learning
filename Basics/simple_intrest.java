c9055c06aa51787f194005be78245ab7aa4ab1eb
karthick h <karthykarthick>
2023-06-07 12:12:39 +0530

    added simple intrest pgm

---
+++ Basics/simple_intrest.java
@@ -0,0 +1,18 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class simple_intrest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your principal Amount: ");
+        int amount = in.nextInt();
+        System.out.print("Enter your Time: ");
+        int time = in.nextInt();
+        System.out.print("Enter your Intrest: ");
+        float intrest = in.nextFloat();
+
+
+        System.out.println("your simple inrest is " + (amount * time * intrest)/100);
+    }
+}
