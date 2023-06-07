2e8f3ed881a84740859b8c2ea75541767b00b12c
karthick h <karthykarthick>
2023-06-07 11:28:37 +0530

    2. added sum of two numbers in java

---
+++ Basics/twoNumbers.java
@@ -0,0 +1,17 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class twoNumbers {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        int number_one = in.nextInt();
+        
+        System.out.println("Enter the second number: ");
+        int number_two = in.nextInt();
+
+        System.out.println("Result: " + (number_one + number_two));
+    }
+}
