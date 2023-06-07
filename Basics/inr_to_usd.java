6bc59de67fabf9e92fc8dc88d8d7a75335089992
karthick h <karthykarthick>
2023-06-07 13:29:11 +0530

    added inr to usd to conversion in java

---
+++ Basics/inr_to_usd.java
@@ -0,0 +1,13 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class inr_to_usd {
+    public static void main(String[] args) {
+        float USD = 82.52f;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your amount in Rupess: ");
+        int inr = in.nextInt();
+        System.out.println("USD:"+ inr/USD);
+    } 
+}
