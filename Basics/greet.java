a892dcaba0190a9cbd81bd4da87770b13c2a0e03
karthick h <karthykarthick>
2023-06-07 12:06:18 +0530

    added greet pgm in java

---
+++ Basics/greet.java
@@ -0,0 +1,12 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class greet {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your Name:");
+        String name = in.nextLine();
+        System.out.println("Have a Nice Day! "+name);
+    }
+}
