b4aafee696978cdd96f0a2072eb4af725cd00842
karthick h <karthykarthick>
2023-06-13 09:23:49 +0530

    added parameter function in java

---
+++ functions/sum.java
@@ -0,0 +1,17 @@
+package functions;
+
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+       
+           int res = greet(300,250);
+            System.out.println(res);
+        
+    }
+    static int greet(int a,int b){
+        int sum = a + b;
+        return sum;
+        
+    }
+}
