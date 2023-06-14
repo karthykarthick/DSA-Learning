bf8e1528ba097e47f34976e6c80f82c6ba522b05
karthick h <karthykarthick>
2023-06-14 13:09:04 +0530

    two dimensional array

---
+++ Arrays/twod_array.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class twod_array {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[3][3];
+
+        // 2d arr input
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                arr[row][col] = in.nextInt();
+            }
+        }
+
+        // 2d arr output
+
+        // for (int row = 0; row < arr.length; row++) {
+        //     for (int col = 0; col < arr[row].length; col++) {
+        //         System.out.print(arr[row][col] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        for(int[] a:arr){
+           System.out.println(Arrays.toString(a));
+        }
+    }
+}
