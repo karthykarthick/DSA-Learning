7ed67751a96df57de62f033194e56fbdfddef773
karthick h <karthykarthick>
2023-06-14 13:09:28 +0530

    swapping two indexes in an array

---
+++ Arrays/swap_array.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class swap_array {
+
+  public static void main(String[] args) {
+  
+    int arr[] = {1,2,3,4,5,6,7,8,9};
+    swap(arr,0,8);
+
+   System.out.println(Arrays.toString(arr));
+
+
+  }
+
+  static void swap(int[] arr,int index1,int index2){
+    int temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+  }
+  
+
+}
